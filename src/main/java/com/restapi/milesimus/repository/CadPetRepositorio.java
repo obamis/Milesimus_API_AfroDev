b634373cabce0d19833e90fed70cb62102aaa75d
Bruno <brunofellipe20@gmail.com>
2020-12-05 12:04:34 -0300

    Primeiro Commit
    API para cadastro de pets em "creche"

---
+++ src/main/java/com/restapi/milesimus/repository/CadPetRepositorio.java
@@ -0,0 +1,11 @@
+package com.restapi.milesimus.repository;
+import com.restapi.milesimus.model.CadPet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CadPetRepositorio extends JpaRepository<CadPet, Long> {
+
+
+
+
+
+}
